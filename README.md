13b77c68e5e5f079fab4646e4debdc9babb395a3
HaykTarkhanyan <hayk_tarkhanyan@outlook.com>
2022-01-19 19:34:41 +0400

    grdfs

---
+++ README.md
@@ -0,0 +1,1 @@
+Package to automatically generate the most suitable plot for your data
--- setup.py
+++ setup.py
@@ -9,11 +9,20 @@ setup(
   author = 'Flomasterner',                   # Type in your name
   author_email = 'flo.master@pingisht.metric',      # Type in your E-Mail
   url = 'https://github.com/narine998',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/HaykTarkhanyan/flomasterplot/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['plotly', 'data frame', 'automation'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'turtle',
+          'pandas',
+          'streamlit', 
+          'plotly', 
+          'traitlets',
+          'pillow',
+          'colorthief',
+          'urllib',
+          'io',
+          'matplotlib',
+          'wordcloud', 
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
