a5996049c15dc80814b89400c81f7b1719b8f4d1
HaykTarkhanyan <hayk_tarkhanyan@outlook.com>
2022-01-19 19:11:25 +0400

    for package

--- flomaster/app.py
+++ flomaster/app.py
@@ -1,4 +1,5 @@
 import os
+from turtle import color
 import pandas as pd
 import streamlit as st
 import plotly.express as px
@@ -8,6 +9,10 @@ from traitlets.traitlets import default
 from col_type_detector import *
 from plots import *
 from helpers import *
+from colorthief import ColorThief
+
+from PIL import Image
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 DEFAULT_FILE = 'Iris.csv'
@@ -15,12 +20,23 @@ DEFAULT_FILE = 'Iris.csv'
 ONE_NUMERIC = ['Histogram', 'Distplot']
 ONE_CATEOGIRCAL = ['Donut', 'Pie', 'Histogram']
 TWO_NUMERIC = ["Scatter", "Scatter plot with margins", "2D density plot", \
-               "Distplot", "Histogram"]
+               "Distplot", "Histogram", "Basic Stats"]
 TWO_NUMERIC_SORTED = ['Connected Scatter', "Area plot", "Line plot"]
 
-ONE_CATEOGIRCAL_ONE_NUMERICAL = ['Box', "Violin"]
+ONE_CATEOGIRCAL_ONE_NUMERICAL = ['Box', "Violin", "Basic Stats"]
 
-st.title("Flomaster plot")
+TWO_CATEGORICAL = ['Cross tab', "Stacked bar"]
+ONE_DATETIME_ONE_NUMERIC = ['Connected Scatter']
+
+
+# url_logo = st.sidebar.text_input('select your logo or provide a url', "")
+# if url_logo:
+#     color_pallete = get_color(url_logo, 2)
+
+image = Image.open('logo.jpg')
+
+st.image(image)
+
 
 url = st.sidebar.text_input('Input url to the .csv file', "None")
 uploaded_file = st.sidebar.file_uploader("Choose a file")
@@ -33,11 +49,6 @@ else:
     data_path = os.path.join('data_samples',DEFAULT_FILE)
     
 df = pd.read_csv(data_path)
-# if uploaded_file and url == "None":
-#     df = pd.read_csv(uploaded_file)
-
-# else:
-#     df = pd.read_csv(os.path.join('data_samples',DEFAULT_FILE))
 
 data_types = get_column_types(df, num_unique_categories=2)
 
@@ -46,25 +57,42 @@ st.markdown("## Data Preview")
 st.write(df.head(100))
 
 x = st.sidebar.selectbox('Select x axis', ["None"] + list(df.columns))
-y = st.sidebar.multiselect("Select columns to use as y axis", ['None'] + list(df.columns), default=["None"])
+y = st.sidebar.multiselect("Select columns to use as y axis", ['None'] + list(df.columns))
 group_by = st.sidebar.selectbox("Select column representing group", ['None'] + list(df.columns))
+
+if group_by == "None":
+    group_by = None
+
+if y == []:
+    y = ["None"]
 
 x_dtype = get_data_type_for_given_feature(data_types, x)
 st.write(f"{x} dtype is {x_dtype}")
 y_dtype = get_data_type_for_given_feature(data_types, y[0])
 st.write(f"{y[0]} dtype is {y_dtype}")
 
+# adding labels and axis names
+x_axis = st.sidebar.text_input("Please input x axis name", x)
+y_axis = st.sidebar.text_input("Please input y axis name", y[0])
+
+if y_axis == "None":
+    y_axis = ""
+
+title = st.sidebar.text_input("Please input title name", f"{x_axis} vs {y_axis}")
+
 
 # one feature
 if x != "None" and y[0] == 'None':
     if x_dtype == 'numeric':
         plot_type = st.selectbox('Select type of the plot', ONE_NUMERIC)
-        fig = one_numeric(df, x, plot_type)
+        fig = one_numeric(df, x, group_by, plot_type)
+        add_labels_to_fig(fig, x_axis, y_axis, title)
         st.plotly_chart(fig)
 
     if x_dtype == 'categorical':
         plot_type = st.selectbox('Select type of the plot', ONE_CATEOGIRCAL)
-        fig = one_categoric(df, x, plot_type)
+        fig = one_categoric(df, x, group_by, plot_type)
+        add_labels_to_fig(fig, x_axis, y_axis, title)        
         st.plotly_chart(fig)
 
     if x_dtype == 'texts':
@@ -89,13 +117,38 @@ if x != "None" and y[0] != 'None':
 
                 
 
-        fig = two_numeric(df, x, y[0], plot_type)
+        fig = two_numeric(df, x, y[0], group_by, plot_type)
+        add_labels_to_fig(fig, x_axis, y_axis, title)        
         st.plotly_chart(fig)
-    # one numeric one categoric
+    # /////////////////////////////////////////////////////////
+
+    #one numeric one categoric
     if x_dtype == "categorical" and y_dtype == 'numeric':
         plot_type = st.selectbox('Select type of the plot', ONE_CATEOGIRCAL_ONE_NUMERICAL)
 
-        fig = one_numeric_one_categorical(df, x, y, plot_type)
+        fig = one_numeric_one_categorical(df, x, y, group_by, plot_type)
+        add_labels_to_fig(fig, x_axis, y_axis, title)        
+
         st.plotly_chart(fig)
 
-# st.write(get_column_types(df))
+    # two categoricals
+    if x_dtype == "categorical" and y_dtype == 'categorical':
+        plot_type = st.selectbox('Select type of the plot', TWO_CATEGORICAL)
+        if plot_type == 'Cross tab':
+            df_cross = two_categorical(df, x, y[0], plot_type)
+            st.write(df_cross)
+        if plot_type == "Stacked Bar":
+            add_labels_to_fig(fig, x_axis, y_axis, title)            
+            st.plotly_chart(fig)
+
+    # one datetime one numeric
+    if x_dtype == "datetime" and y_dtype == 'numeric':
+
+        if check_list_in_list(list(df.columns), ['Date', "Open", "High", "Low", "Close"]):
+            ONE_DATETIME_ONE_NUMERIC += ["Stock price"]
+        plot_type = st.selectbox('Select type of the plot', ONE_DATETIME_ONE_NUMERIC)
+        fig = one_datetime_one_numeric(df, x, y, group_by,plot_type)
+        add_labels_to_fig(fig, x_axis, y_axis, title)        
+        st.plotly_chart(fig)
+
+    
--- helpers.py
+++
@@ -1,7 +0,0 @@
-def get_data_type_for_given_feature(data_types, feature):
-    for k in list(data_types.keys()):
-        if feature in data_types[k]:
-            if k == 'remove_cols' or k == 'binary':
-                k = 'categorical'
-            return k 
-    return 'None'
